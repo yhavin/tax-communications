dd7166484a91fce1e69337995aa60483fa384dd7
yhavin <y.havin@gmail.com>
2024-07-23 16:35:08 -0400

    Update main.py formatting for clarity

--- main.py
+++ main.py
@@ -10,21 +10,31 @@ from logger import logger
 
 
 if __name__ == "__main__":
-    sender = "yhavin@beitel.com"
-    internal_recipients = ["akessler@beitel.com", "aschmarlberg@beitel.com", "rdancona@beitel.com"]
-    tax_year = "2023"
-    test_mode = True
-    email_limit = 1
-    reset_status = True
-    run_send_emails = False
+    SENDER = "yhavin@beitel.com"
+    INTERNAL_RECIPIENTS = ["akessler@beitel.com", "aschmarlberg@beitel.com", "rdancona@beitel.com"]
+    TAX_YEAR = "2023"
+    TEST_MODE = True
+    EMAIL_LIMIT = 1
+    SKIP_CACHE_LOAD = True
+    RESET_STATUS = True
 
-    k = K1BatchProcessor(sender=sender, internal_recipients=internal_recipients, tax_year=tax_year, test_mode=test_mode, email_limit=email_limit, reset_status=reset_status)
+    RUN_SEND_EMAILS = False
+
+    k = K1BatchProcessor(
+        sender=SENDER, 
+        internal_recipients=INTERNAL_RECIPIENTS, 
+        tax_year=TAX_YEAR, 
+        test_mode=TEST_MODE, 
+        email_limit=EMAIL_LIMIT, 
+        skip_cache_load=SKIP_CACHE_LOAD,
+        reset_status=RESET_STATUS
+    )
     k.extract_entities()
     k.print_k1_array()
     k.match_files_and_keys()
     
-    if run_send_emails:
-        email_number = email_limit if email_limit is not None else "max"
+    if RUN_SEND_EMAILS:
+        email_number = EMAIL_LIMIT if EMAIL_LIMIT is not None else "max"
         confirmation = input(f"CAUTION: Are you sure you want to send {email_number} email{"s" if email_number != 1 else ""}? (y/n) ")
         if confirmation.lower() == "y":
             print("\n")
