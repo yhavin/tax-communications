f86dc30892b7ed07f6a6222d81da5426f62cd27b
yhavin <y.havin@gmail.com>
2024-07-24 14:57:36 -0400

    Update entry point to use config.py (untracked but has pytemplate)

--- main.py
+++ main.py
@@ -7,18 +7,22 @@ Author: Yakir Havin
 
 from k1_processor import K1BatchProcessor
 from logger import logger
+try:
+    from config import config
+except ImportError:
+    print("`config.py` not found. Please create it from `config.pytemplate`.")
+    exit(1)
 
 
 if __name__ == "__main__":
-    SENDER = "yhavin@beitel.com"
-    INTERNAL_RECIPIENTS = ["akessler@beitel.com", "aschmarlberg@beitel.com", "rdancona@beitel.com"]
-    TAX_YEAR = "2023"
-    TEST_MODE = True
-    EMAIL_LIMIT = 1
-    SKIP_CACHE_LOAD = True
-    RESET_STATUS = True
-
-    RUN_SEND_EMAILS = False
+    SENDER = config["sender"]
+    INTERNAL_RECIPIENTS = config["internal_recipients"]
+    TAX_YEAR = config["tax_year"]
+    TEST_MODE = config["test_mode"]
+    EMAIL_LIMIT = config["email_limit"]
+    SKIP_CACHE_LOAD = config["skip_cache_load"]
+    RESET_STATUS = config["reset_status"]
+    RUN_SEND_EMAILS = config["run_send_emails"]
 
     k = K1BatchProcessor(
         sender=SENDER, 
@@ -30,16 +34,16 @@ if __name__ == "__main__":
         reset_status=RESET_STATUS
     )
     k.extract_entities()
-    k.print_k1_array()
+    # k.print_k1_array()
     k.match_files_and_keys()
     
     if RUN_SEND_EMAILS:
-        email_number = EMAIL_LIMIT if EMAIL_LIMIT is not None else "max"
-        confirmation = input(f"CAUTION: Are you sure you want to send {email_number} email{"s" if email_number != 1 else ""}? (y/n) ")
+        email_number = EMAIL_LIMIT if EMAIL_LIMIT is not None else "all available"
+        confirmation = input(f"CAUTION ({"test_mode" if TEST_MODE else "live_mode"}): Are you sure you want to send {email_number} email{"s" if email_number != 1 else ""} from {SENDER}? (y/n) ")
         if confirmation.lower() == "y":
             print("\n")
             k.send_emails()
         else:
-            print("Email sending aborted by user.")
+            print("\nEmail sending aborted by user.")
 
     logger.close()
