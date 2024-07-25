0a1c15be0e596e4daf654b93f6b04229319f5014
yhavin <y.havin@gmail.com>
2024-07-25 14:37:44 -0400

    Remove unnecessary variables in main.py

--- main.py
+++ main.py
@@ -15,27 +15,19 @@ except ImportError:
 
 
 if __name__ == "__main__":
-    SENDER = config["sender"]
-    INTERNAL_RECIPIENTS = config["internal_recipients"]
-    TAX_YEAR = config["tax_year"]
-    TEST_MODE = config["test_mode"]
-    EMAIL_LIMIT = config["email_limit"]
-    SKIP_CACHE_LOAD = config["skip_cache_load"]
-    RUN_SEND_EMAILS = config["run_send_emails"]
-
     k = K1BatchProcessor(
-        sender=SENDER, 
-        internal_recipients=INTERNAL_RECIPIENTS, 
-        tax_year=TAX_YEAR, 
-        test_mode=TEST_MODE, 
-        email_limit=EMAIL_LIMIT, 
-        skip_cache_load=SKIP_CACHE_LOAD
+        sender=config["sender"], 
+        internal_recipients=config["internal_recipients"], 
+        tax_year=config["tax_year"], 
+        test_mode=config["test_mode"], 
+        email_limit=config["email_limit"], 
+        skip_cache_load=config["skip_cache_load"]
     )
     k.extract_entities()
     # k.print_k1_array()
     k.match_files_and_keys()
     
-    if RUN_SEND_EMAILS:
+    if config["run_send_emails"]:
         k.send_emails()
 
     logger.close()
