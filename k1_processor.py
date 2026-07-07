3c3bb2b394d85b711cd31bb2638c31614d80c3b7
yhavin <y.havin@gmail.com>
2026-07-07 14:35:15 -0400

    Update receiving entity regex rules

--- k1_processor.py
+++ k1_processor.py
@@ -211,7 +211,8 @@ class K1BatchProcessor:
             (r"\bnachal\b", True),
             (r"\bgrove\b", True),
             (r"\bc/o\b", True),
-            (r"\bplaza\b", True)
+            (r"\bplaza\b", True),
+            (r"\bparkway\b", True)
         ]
 
         for index, k1_info in enumerate(k1_files_to_extract):
